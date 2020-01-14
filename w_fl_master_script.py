f667523a80244142dd32990ffcfbc4387587a7cb
camante <Christopher.Amante@colorado.edu>
2020-01-14 14:52:01 -0700

    \"${1}\"

--- w_fl_master_script.py
+++ w_fl_master_script.py
@@ -307,5 +307,3 @@ create_dem_cmd='create_dem.sh ' + name_cell_extents_dem + ' ' + dem_dlist + ' ' 
 #create_dem.sh /media/sf_E_win_lx/COASTAL_Act/camante/w_fl/software/gridding/name_cell_extents_dem.csv /media/sf_E_win_lx/COASTAL_Act/camante/w_fl/software/gridding/w_fl.datalist /media/sf_E_win_lx/COASTAL_Act/camante/w_fl/data/bathy/bathy_surf/tifs 5
 #create_dem.sh /media/sf_E_win_lx/COASTAL_Act/camante/w_fl/software/gridding/name_cell_extents_dem.csv /media/sf_E_win_lx/COASTAL_Act/camante/w_fl/software/gridding/w_fl.datalist 5
 os.system(create_dem_cmd)
-
-print "Tested out git2"

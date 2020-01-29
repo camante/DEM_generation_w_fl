4873ba76579e412445ce018e3cb4368a4a41a1fb
camante <Christopher.Amante@colorado.edu>
2020-01-28 17:25:48 -0700

    complete code for Matt S

--- git_push.sh
+++ git_push.sh
@@ -2,16 +2,17 @@
 function help () {
 echo "git_push - Script that pushes code to git with user provided description."
 	echo "Usage: $0 description"
-	echo "* description: user provides general description on updates to code"
+	echo "* description: user provides general description on updates to code, surrounded by quotes"
 }
+
 #show help if not
 if [ ${#@} == 1 ]; 
 then
 #User inputs
-description='\"${1}\"'
-#echo $description
+description=${1}
+
 sudo git add --all
-sudo git commit -m $description
+sudo git commit -m "$description"
 sudo git push
 
 else
